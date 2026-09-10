10f754851c8466e03969ce6ea975193307994d0e
Kim0900 <kjunhun0900@gmail.com>
2026-09-10 10:05:42 +0900

    v0.2.2: build.gradle.kts - 원본 이미지 적용

--- GNSSKeeper/app/build.gradle.kts
+++ GNSSKeeper/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "kr.magi.gnsskeeper"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     signingConfigs {
