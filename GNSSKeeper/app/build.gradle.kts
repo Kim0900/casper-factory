71c03113be24a2614a84b9740a68ab81f1a17f26
Kim0900 <kjunhun0900@gmail.com>
2026-09-10 10:49:16 +0900

    v0.2.4: build.gradle.kts - 투명 PNG 적용

--- GNSSKeeper/app/build.gradle.kts
+++ GNSSKeeper/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "kr.magi.gnsskeeper"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
     }
 
     signingConfigs {
